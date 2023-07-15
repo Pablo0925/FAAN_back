50baf8e2200af86225bfa31be8ccb038750eb035
Alejo2412qwe <111301530+Alejo2412qwe@users.noreply.github.com>
2023-07-14 20:41:07 -0500

    Add files via upload

---
+++ src/main/java/com/proyecto/faan/FaanApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.faan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaanApplication.class, args);
+	}
+
+}
