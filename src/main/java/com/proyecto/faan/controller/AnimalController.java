5f8a1a7cabbdf18f35d840c135c3cee339ab952f
EstebanBacuilima <davidbacuilima13@gmail.com>
2023-07-18 09:38:33 -0500

    New Filter Animlas

--- src/main/java/com/proyecto/faan/controller/AnimalController.java
+++ src/main/java/com/proyecto/faan/controller/AnimalController.java
@@ -7,6 +7,12 @@ import com.proyecto.faan.peyload.PeyloadNumeroAdopcionRaza;
 import com.proyecto.faan.service.AnimalService;
 import com.proyecto.faan.service.generic.GenericService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -30,5 +36,18 @@ public class AnimalController extends GenericControllerImpl<Animal, Integer> {
     @GetMapping("/lista/PeyloadNumeroAdopcionFecha")
     public List<PeyloadNumeroAdopcionFecha> findByAnimalPeyloadFechaAdopcion() {
         return animalService.findByAnimalPeyloadFechaAdopcion();
+    }
+
+    @GetMapping("/findBynameAnimalOrPlacaAnimal/{filter}")
+    public ResponseEntity<?> findBynameAnimalOrPlacaAnimal(@PathVariable("filter") String filter, @PageableDefault(page = 0, size = 3, direction = Sort.Direction.ASC) Pageable pageable) {
+        try {
+            Page<Animal> animalFind = animalService.findByNombreAnimalOrPlacaAnimal(filter, pageable);
+            if(animalFind != null){
+                return new ResponseEntity<>(animalFind, HttpStatus.OK);
+            }
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
--- src/main/java/com/proyecto/faan/service/AnimalService.java
+++ src/main/java/com/proyecto/faan/service/AnimalService.java
@@ -4,6 +4,8 @@ import com.proyecto.faan.model.Animal;
 import com.proyecto.faan.peyload.PeyloadNumeroAdopcionFecha;
 import com.proyecto.faan.peyload.PeyloadNumeroAdopcionRaza;
 import com.proyecto.faan.service.generic.GenericService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -11,4 +13,6 @@ public interface AnimalService extends GenericService<Animal, Integer> {
     List<PeyloadNumeroAdopcionRaza> findByAnimalPeyload();
 
     List<PeyloadNumeroAdopcionFecha> findByAnimalPeyloadFechaAdopcion();
+
+    public Page<Animal> findByNombreAnimalOrPlacaAnimal(String filter, Pageable pageable);
 }
